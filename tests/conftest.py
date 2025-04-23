1ef66d1249dcfc07c5316a092508bdd240d940bc
Nailia Aleshicheva <gilmanova-nailya@mail.ru>
2025-04-24 00:32:17 +0300

    дз 11: селеноид, дженкинс и аллюр на демокуа

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 
-#import attach
-import allure_attach as attach
+import attach
+#import allure_attach as attach
 
 load_dotenv()
 
